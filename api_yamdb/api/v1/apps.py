54ad6307364537f2678439afb87a3191b66f5601
Семенова Елизавета <thealeeeaaa23@gmail.com>
2023-04-11 17:26:15 +0400

    start again

--- api_yamdb/api/v1/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiConfig(AppConfig):
-    """Base configuration class for the API app."""
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
